b4e03d5744caa8995958fbe1eec0e7caae9ccf48
Yurins <mijhaelyurinscoaquirajusto@gmail.com>
2025-05-04 12:07:30 -0500

    carpetas inapropiadas

--- ms_catalogo/src/main/java/com/mycj/student/mycjStudentService/repository/EstudianteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycj.student.mycjStudentService.repository;
-
-import com.mycj.student.mycjStudentService.entity.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstudianteRepository extends JpaRepository<Estudiante, Integer> {
-}
