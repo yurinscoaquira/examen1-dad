0912c80a1fa2465bdfb6544d0473cdb1f2a0b543
Yurins <mijhaelyurinscoaquirajusto@gmail.com>
2025-05-15 06:43:36 -0500

    realizando cambios

---
+++ dmb-auth/src/test/java/org/example/dmbauth/DmbAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dmbauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmbAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
